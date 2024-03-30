a75624d2046d1596149847f4f8364a35b94e9423
Wilson Li <wilsonlmk23@gmail.com>
2024-03-31 02:28:19 +0800

    Add file for Week-3

---
+++ Week-1/Assignment-3/assignment-3.js
@@ -0,0 +1,26 @@
+function countAandB(input) {
+    let ans = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === "a" || input[i] === "b") {
+            ans += 1;
+        }
+    }
+    return ans;
+}
+
+function toNumber(input) {
+    // your code here
+    let ans = [];
+    for (let i = 0; i < input.length; i++) {
+        ans.push(input[i].charCodeAt(0) - 96);
+    }
+    return ans;
+}
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ lettersand 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
