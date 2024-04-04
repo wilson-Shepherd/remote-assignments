a48321e61d083fb2999f90c5697c77c32f1c1caf
wilson <wilsonlmk23@gmail.com>
2024-04-04 23:45:36 +0800

    finished Week-2 assignment-2.js

--- Week-2/Assignment-2/assignment-2.js
+++ Week-2/Assignment-2/assignment-2.js
@@ -1,22 +1,28 @@
 function avg(data) {
     // your code here
+    let sum = 0;
+    for (let i = 0; i < data.size; i++) {
+        sum += data.products[i].price;
     }
-    console.log(
+    return sum / data.size;
+}
+
+console.log(
     avg({
-    size: 3,
-    products: [
-    {
-    name: 'Product 1',
-    price: 100,
-    },
-    {
-    name: 'Product 2',
-    price: 700,
-    },
-    {
-    name: 'Product 3',
-    price: 250,
-    },
-    ],
+        size: 3,
+        products: [
+            {
+                name: 'Product 1',
+                price: 100,
+            },
+            {
+                name: 'Product 2',
+                price: 700,
+            },
+            {
+                name: 'Product 3',
+                price: 250,
+            },
+        ],
     })
-    ); // should print the average price of all products
+); // should print the average price of all products
