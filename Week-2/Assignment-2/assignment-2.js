7576c231652f1fc7000468eac22c425440418387
wilson <wilsonlmk23@gmail.com>
2024-04-04 23:37:47 +0800

    Init Week-2 assignment-2

---
+++ Week-2/Assignment-2/assignment-2.js
@@ -0,0 +1,22 @@
+function avg(data) {
+    // your code here
+    }
+    console.log(
+    avg({
+    size: 3,
+    products: [
+    {
+    name: 'Product 1',
+    price: 100,
+    },
+    {
+    name: 'Product 2',
+    price: 700,
+    },
+    {
+    name: 'Product 3',
+    price: 250,
+    },
+    ],
+    })
+    ); // should print the average price of all products
