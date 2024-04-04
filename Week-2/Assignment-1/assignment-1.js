0597ed1e44e1b2dfd77be2d006b41009956bd815
wilson <wilsonlmk23@gmail.com>
2024-04-04 23:33:52 +0800

    finished assignment-1

---
+++ Week-2/Assignment-1/assignment-1.js
@@ -0,0 +1,27 @@
+function max(numbers) {
+    let maxNum = 0;
+    // your code here, for-loop method preferred
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] > maxNum) {
+            maxNum = numbers[i];
+        }
+    }
+    return maxNum;
+}
+
+function findPosition(numbers, target) {
+    // your code here, for-loop method preferred
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([5, 2, 7, 1, 6])); // should print 7
+console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
+console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
+console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position)
+console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
