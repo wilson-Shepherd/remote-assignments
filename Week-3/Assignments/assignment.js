68d4542f5db035723f96d9da36286d9315d26859
wilson <wilsonlmk23@gmail.com>
2024-04-14 19:16:59 +0800

    feat: Implemented cookie handling for user's name

--- Week-3/Assignments/assignment.js
+++ Week-3/Assignments/assignment.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
 app.use(express.static(__dirname)); // for sum.html usage
+app.use(cookieParser()); // for cookie usage
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // def root路由 ('/')
 app.get("/", (req, res) => {
@@ -9,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("<h1>My name is Wilson Li ^_^</h1>");
 });
 
-// def data路由 ('/')
+// def data路由 ('/data')
 app.get("/data", (req, res) => {
   // 獲取使用者網址輸出的數字
   const number = req.query.number;
@@ -22,24 +27,46 @@ app.get("/data", (req, res) => {
     return res.send("Wrong Parameter");
   }
 
+  // 處理負數的情況
   if (parseInt(number) < 0) {
     return res.send("It has to be positive integer");
   }
 
-  // 計算 1 到 number 的總和
+  // 用 pascal number formula
   const sum = (parseInt(number) * (parseInt(number) + 1)) / 2;
-
-  // 向客戶端發送計算結果
   res.send(`${sum}`);
 
   /* 
-  Optional: 假設輸入的 N 是超大的數字，如果同時有多個用戶一起同時訪問，
-  可能會造成伺服器效能過高，從而影響伺服器的穩定性。
+  Optional: 假設輸入的 N 是超級大的數字，同時有多個用戶一起同時訪問這個超大的 N，
+  有機會造成伺服器效能過高，從而影響伺服器的穩定性，用戶可能會出現斷開連接的情況。
   */
 });
 
-// 啟動伺服器並監聽指定的端口號
+// def myName路由 ('/myName')
+app.get("/myName", (req, res) => {
+  const username = req.cookies.username;
+  if (username) {
+    res.send(`<h1>${username}!</h1>`);
+  } else {
+    res.send(`
+      <h1>Please enter your name:</h1>
+      <form action="/trackName" method="POST">
+        <input type="text" name="username" />
+        <button type="submit">Submit</button>
+      </form>
+    `);
+  }
+});
+
+// def trackName路由 (‘/trackName’)
+app.post("/trackName", (req, res) => {
+  const { username } = req.body;
+  if (username) {
+    res.cookie("username", username);
+  }
+  res.redirect("/myName");
+});
+
 app.listen(port, () => {
-  // 當伺服器啟動後，在控制台中記錄一條消息
   console.log(`Server is running, visit: http://localhost:${port}`);
 });
