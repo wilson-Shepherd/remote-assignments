0735ddfd28c463cf39143b82ad58c5c0d178c3bd
wilson <wilsonlmk23@gmail.com>
2024-04-14 18:08:33 +0800

    feat: Add /data route for handling input number

--- Week-3/Assignments/assignment.js
+++ Week-3/Assignments/assignment.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// 定義root路由 ('/')
-// 當有人訪問 http://localhost:3000/ 時，這個路由會被觸發
+// def root路由 ('/')
 app.get("/", (req, res) => {
-  // 向客戶端發送包含以下 HTML 內容的回應
+  // 向 Client 發送 respond
   res.send("<h1>My name is Wilson Li ^_^</h1>");
 });
 
-// 定義data路由 ('/')
-app.get("/", (req, res) => {
-  // 獲取使用者輸出的數字
+// def data路由 ('/')
+app.get("/data", (req, res) => {
+  // 獲取使用者網址輸出的數字
   const number = req.query.number;
 
   if (!number) {
@@ -26,8 +25,14 @@ app.get("/", (req, res) => {
   const sum = (parseInt(number) * (parseInt(number) + 1)) / 2;
 
   // 向客戶端發送計算結果
-  res.send(`The result of 1+2+....+${number} is ${sum}`);
+  res.send(`${sum}`);
+
+  /* 
+  Optional: 假設輸入的 N 是超大的數字，如果同時有多個用戶一起同時訪問，
+  可能會造成伺服器效能過高，從而影響伺服器的穩定性。
+  */
 });
+
 // 啟動伺服器並監聽指定的端口號
 app.listen(port, () => {
   // 當伺服器啟動後，在控制台中記錄一條消息
